fb1d8f3154153af6e514e3919c4215fd80925e1f
SkyCHarris <skyler.iqeq@gmail.com>
2023-08-27 20:14:13 -0600

    expressions, integers, floating-point, string data, string concantenation, replication

---
+++ chapter_1/pt_1.py
@@ -0,0 +1,42 @@
+# Operators
+
+# ** | Exponent | 2 ** 3 | 8
+# % | Modulus/Remainder | 22 % 8 | 6
+# // | Integer Division / Floored Quotient | 22 // 8 | 2
+# / | Division | 22 / 8 | 2.75
+# * | Multiiplication | 3 * 5 | 15
+# - | Subtraction | 5 - 2 | 3
+# + | Addition | 2 + 2 | 4
+
+print(2 + 3 * 6)
+print((2+3) * 6)
+print(48565878 * 578453)
+print(2 ** 8)
+print(23 / 7)
+print(23 // 7)
+print(22 % 7)
+print(2    +    2)
+print((5-1) * ((7 + 1) / (3 - 1)))
+
+# Integer, Floating-Point, and String Data Types
+
+# -Expressions are just values combined with operators, and always evaluate down to a single value
+# -A 'data type' is a category for values, and every value belongs to exactly one data type
+
+# Integers | -2, -1, 0, 1, 2, 3, 4, 5
+# Floating-point Numbers | -1.25, -1,0, -0.5, 0.0, 0.5, 1.0, 1.25
+# Strings | 'a', 'aa', 'aaa', 'Hello!', '11 cats'
+
+# String Concatenation and Replication
+
+# -The meaning of an operator may change based on the data types of the values next to it
+# -ex. + can be addition for integers/floats, or concatenation for strings
+
+print('Alice' + 'Bob')
+
+# The * operator can be used for:
+# -Multiplying two integer or floating-point values
+# -One string value and one ineteger value (string replication operator)
+
+print('Alice' * 5)
+
---
+++ chapter_1/pt_2.py
@@ -0,0 +1,12 @@
+# Storing Values in Variables
+
+# -A 'variable' is like a box in the computer's memory where you can store a single value
+# -If you want to use the result of an evaluated expression later in your program, you can save it inside a variable
+
+# Assignment Statements
+
+# -Store values in variables using an 'assignment statement'
+# -Consists of: variable name, equal sign (assignment operator), and value to be stored
+
+spam = 40
+print(spam)
