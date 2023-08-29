eafa70a1520203010474b7d2b03b7c16ba933fd6
SkyCHarris <skyler.iqeq@gmail.com>
2023-08-28 18:40:32 -0600

    storing values in variables, assignment statements, variable names

--- chapter_1/pt_2.py
+++ chapter_1/pt_2.py
@@ -10,3 +10,31 @@
 
 spam = 40
 print(spam)
+
+eggs = 2
+print(spam + eggs)
+
+print(spam + eggs + spam)
+
+spam = spam + 2
+print(spam)
+
+# -A variable is 'initialized' (created) the first time a value is stored in it
+# 1. Variable can then be used in an expression
+# 2. When a variable is assigned to a new value...
+# 3. The old value is forgotten
+# -This is called 'overwriting' the variable
+
+spam = 'Hello'
+print(spam)
+
+spam = 'Goodbye'
+print(spam)
+
+# Variable Names
+
+# -A good variable name describes the data it contains
+# Rules:
+# +It can be only one word with no spaces
+# +It can use only letters, numbers, and the underscore _ character
+# +It can't begin with a number
