dc5fe868624d626b1a22ea4794ba33b8e508d841
Sky Harris <skyler.iqeq@gmail.com>
2023-09-08 15:10:15 -0600

    practice problem #12 for flow control chapter

---
+++ flow_control2/practice12.py
@@ -0,0 +1,3 @@
+# What is the difference between range(10), range(0, 10), and range(0, 10, 1) in a for loop?
+
+# The function outputs the same values, however, using two parameters defines the function range, while using three parameters defines the function range AND integer increase
