736b78f6fa09395e878c6ed75358184f656f2955
Sky Harris <skyler.iqeq@gmail.com>
2023-09-08 14:28:40 -0600

    practice problems 6 & 7 for flow control chapter

---
+++ flow_control2/practice6.py
@@ -0,0 +1,8 @@
+# What is the difference between the equal to operator and the assignment operator?
+
+# Assignment operators attach a value to something, like storing a value in a variable.
+# The 'equal to' operator returns the truthiness of an expression, like True == True (True)
+
+# ACTUAL
+
+# == is the equal to operator that compares two values and evalutes to a Boolean, while = is the assignment operator that stores a value in a variable
---
+++ flow_control2/practice7.py
@@ -0,0 +1,8 @@
+# Explain what a condition is and where you would use one.
+
+# An example of a condition is an if/else statement, where a program runs through if, if elses, and else, evaluating each line based on its truthiness
+
+# ACTUAL
+
+# A condition is an expression used in a flow control statement that evalutes to a Boolean value
+
