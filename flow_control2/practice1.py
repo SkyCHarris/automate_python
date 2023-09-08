f9e732e633a092e49863d998921c2036e23e5dae
Sky Harris <skyler.iqeq@gmail.com>
2023-09-08 13:43:24 -0600

    practice problem #1 in flow control chapter

---
+++ flow_control2/practice1.py
@@ -0,0 +1,6 @@
+# What are the two values of the Boolean data type? How do you write them?
+
+True
+False
+
+# Write these values beginning with a capital letter, and they are the furthest an expression can be simplified to
