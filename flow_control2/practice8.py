a3475418e01310633052f9221caf35b7707a53a0
Sky Harris <skyler.iqeq@gmail.com>
2023-09-08 15:02:18 -0600

    practice problem #8 for flow control chapter

---
+++ flow_control2/practice8.py
@@ -0,0 +1,15 @@
+# Identify the three blocks in this code:
+
+spam = 0 # Variable
+if spam == 10: # Condition statement | if/else statement
+    print('eggs')
+    if spam > 5:
+        print('bacon')
+    else:
+        print('ham')
+    print('spam')
+print('spam')
+
+# ACTUAL
+
+# The three blocks are everything insde the if statement, and the lines print ('bacon') and print('ham')
