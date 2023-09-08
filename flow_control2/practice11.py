11d089a70bcd006182a5b90aeb7b44a43219e5de
Sky Harris <skyler.iqeq@gmail.com>
2023-09-08 15:07:55 -0600

    practice problem #11 for flow control chapter

---
+++ flow_control2/practice11.py
@@ -0,0 +1,8 @@
+# What is the difference between 'break' and 'continue'?
+
+# Break exits out of the loop, while continue goes back to the start of the loop
+
+
+# ACTUAL
+
+# Break statement will move the execution outside and just after a loop. Continue statement will move the execution to the start of the loop
