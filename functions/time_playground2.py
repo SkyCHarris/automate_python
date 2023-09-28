5d4378ffc25879820a32529265165042736becfb
Sky Harris <skyler.iqeq@gmail.com>
2023-09-28 14:03:26 -0600

    python time module documentation

---
+++ functions/time_playground2.py
@@ -0,0 +1,7 @@
+# struct_time
+
+from time import struct_time
+time_tuple = (2019, 2, 26, 7, 6, 55, 1, 57, 0)
+time_obj = struct_time(time_tuple)
+print(time_obj)
+
---
+++ functions/zigzag_program.py
@@ -0,0 +1,30 @@
+# A Short Program: Zigzag
+
+# Let's create a small animation program. It will create a back-and-forth zigzag pattern until the user stops it by pressing the Stop button or CTRL-C
+
+import time, sys
+indent = 0 # How many spaces to indent.
+indentIncreasing = True # Whether the indentation is increasing or not.
+
+try:
+    while True: # The main program loop
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1) # Pause for 1/10 of a second.
+
+        if indentIncreasing:
+            # Increase the number of spaces:
+            indent = indent + 1
+            if indent == 20:
+                # Change direction:
+                indentIncreasing = False
+        else:
+            # Decrease the number of spaces:
+            indent = indent - 1
+            if indent == 0:
+                # Change direction:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()
+
+# 1. First, import the time and sys modules
