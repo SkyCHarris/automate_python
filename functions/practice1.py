b36f7eb45e5afaebd27455ecbff64182b3e0d221
Sky Harris <skyler.iqeq@gmail.com>
2023-09-29 10:39:37 -0600

    practice question 1 in functions chapter

---
+++ functions/practice1.py
@@ -0,0 +1,4 @@
+# Why are functions advantageous to have in your program?
+
+# Functions help to reduce redundancy and automate processes, as well as isolate bugs
+# Functions make programs shorter, easier to read, and easier to update
