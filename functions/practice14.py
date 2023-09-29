778c9c591c40ba1a4c24ec26dd0f0e1d05853771
Sky Harris <skyler.iqeq@gmail.com>
2023-09-29 11:15:11 -0600

    practice question 14 in functions chapter

---
+++ functions/practice14.py
@@ -0,0 +1,5 @@
+# What goes in the try clause? What goes in the except clause?
+
+# The try clause should contain the code block which will execute and may encounter errors
+# The except clause should contain any exceptions, and a message 
+# The code that executes if an error happens goes in the except clause
