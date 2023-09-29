d0fa92ddd045897cfd4f735d7b4eb20b2a47f0f8
Sky Harris <skyler.iqeq@gmail.com>
2023-09-29 11:03:33 -0600

    practice question 7 in functions chapter

---
+++ functions/practice7.py
@@ -0,0 +1,3 @@
+# What is a return value? Can a return value be part of an expression?
+
+# A return value is the value that a function call evalutes to. Like any value, a return value can be used as part of an expression
