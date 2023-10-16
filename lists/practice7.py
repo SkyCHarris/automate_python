584a054e6922d83993fac1622ba57a8c49455372
Sky Harris <skyler.iqeq@gmail.com>
2023-10-16 13:06:23 -0600

    practice question 7 in lists chapter

---
+++ lists/practice7.py
@@ -0,0 +1,5 @@
+# For the following three questions, let's say bacon contains the list [3.14, 'cat', 11, 'cat', True]
+
+# 7. What does bacon.append(99) make the list value in bacon look like?
+
+# [3.14, 'cat', 11, 'cat', True, 99]
