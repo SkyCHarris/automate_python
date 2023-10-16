a656cc5e6cb9490b9ee3e9ffc3ded18bd405af2e
Sky Harris <skyler.iqeq@gmail.com>
2023-10-16 13:31:03 -0600

    playground/sandbox for testing similarities between lists and strings

--- lists/playground.py
+++ lists/playground.py
@@ -1,5 +1,27 @@
+# len()
 spam = 'python'
 eggs = [1, 2, 3]
 
 len(spam)
 len(eggs)
+
+# index
+spam = 'python'
+eggs = [1, 2, 3]
+
+spam[0]
+eggs[0]
+
+# slice
+spam = 'python'
+eggs = [1, 2, 3]
+
+spam[0:3]
+eggs[0:2]
+
+# in not in
+spam = 'python'
+eggs = [1, 2, 3]
+
+'pyt' in spam
+2 in eggs
