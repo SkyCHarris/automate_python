503b6181f2b420574eb77e500fdff04c456c1ef9
Sky Harris <skyler.iqeq@gmail.com>
2023-10-16 12:54:35 -0600

    practice question 3 for lists chapter

---
+++ lists/practice3.py
@@ -0,0 +1,10 @@
+# For the following three questions, let's say spam contains the list ['1', 'b', 'c', 'c']
+
+# 3. What does spam[int(int('3' * 2) // 11)] evalute to?
+
+# Actual:
+# 'd' ->
+# '3' * 2 = '33'
+# int('33') = 33
+# 33 // 11 = 3
+# spam[3] = 'd'
