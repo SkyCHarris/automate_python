ad777c0764fa63866fcd1fbd735aa04e00d5f682
Sky Harris <skyler.iqeq@gmail.com>
2023-10-16 13:33:07 -0600

    practice question 13 in lists chapter

---
+++ lists/practice13.py
@@ -0,0 +1,8 @@
+# What is the difference between lists and tuples?
+
+# 13. Lists are mutable, tuples are immutable
+
+# Actual:
+# Lists are mutable; they can have values added, removed, or changed.
+# Tuples are immutable; they cannot be changed at all
+# Also, tuples are written using parentheses, ( and ), while lists use the square brackets, [ and ].
