c907545dcaa1435d77a25784218e5313933c5e92
Sky Harris <skyler.iqeq@gmail.com>
2023-10-20 13:43:05 -0600

    Practice question 5 in Dicitonaries chapter

---
+++ dictionaries_structuring_data5/practice5.py
@@ -0,0 +1,6 @@
+# 5. If a citionary is stored in spam, what is the difference between the expressions 'cat' in spam and 'cat' in spam.keys()?
+
+# spam = 'cat' is a list item, while spam.keys(cat) is the key value
+
+# Actual
+# There is no difference. The in operator checks whether a value exists as a key in the dictionary
