46508a10ba9d76a611d349db2902a02e27ec7345
Sky Harris <skyler.iqeq@gmail.com>
2023-10-20 13:39:14 -0600

    Practice question 3 in Dictionaries chapter

---
+++ dictionaries_structuring_data5/practice3.py
@@ -0,0 +1,6 @@
+# 3. What is the main difference between a dictionary and a list?
+
+# Dictionaries contain key-value pairs and can thus be used for data structuring, whereas lists only contain a set of items
+
+# Actual
+# The items stored in a dictionary are unordered, while the items in a list are ordered
