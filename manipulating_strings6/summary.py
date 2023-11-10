415e944763c9c51a31714443a366614f688cf43f
Sky Harris <skyler.iqeq@gmail.com>
2023-11-10 15:06:24 -0700

    Summary of Manipulating Strings chapter

---
+++ manipulating_strings6/summary.py
@@ -0,0 +1,15 @@
+
+#! Summary
+
+# Text is a common form of data, and Python comes with many helpful string methods to process text stored in string values
+# Make use of indexing, slicing, and string methods in almost every Python program
+
+# Programs we're writing don't seem sophisticated, no GUIs with images and colorfol text
+# We're displaying text with print() and input()
+# But the user can quickly enter large amounts of text through the clipboard
+    # This provides a useful avenue for writing programs that manipulate massive amounts of text
+# These text-based programs might not have flashy windows or graphics, but they get a lot of useful work done quickly
+
+# Another way to manipulate large amounts of text is reading and writing files directly off the hard drive. Chapter 9
+
+# That's most the basic concepts of Python programming! More to come
