104da7ccaed3d34fcb25691dbf679054f05ed16f
Sky Harris <skyler.iqeq@gmail.com>
2023-11-15 14:20:01 -0700

    Practice 8 for Manipulating Strings chapter

---
+++ manipulating_strings6/practice8.py
@@ -0,0 +1,11 @@
+
+#? What do the following expressions evalute to?
+# 1. 'Remember, remember, the fifth of November.'.split()
+# 2. '-'.join('There can be only one.'.split())
+
+'Remember, remember, the fifth of November.'.split()
+# ['Remember', 'remember', 'the', 'fifth', 'of', 'Novemeber.']
+
+'-'.join('There can be only one.')
+# ['There-can-be-only-one.']
+# ['There]
