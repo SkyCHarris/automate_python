750d3882be0b78cfe8c770b1e3ed774b6dc8a233
Sky Harris <skyler.iqeq@gmail.com>
2023-11-15 10:23:50 -0700

    Practice 6 for Manipulating Strings chapter

---
+++ manipulating_strings6/practice5.py
@@ -0,0 +1,9 @@
+
+#? If you don't want to put \n in your string, how can you write a string with newlines in it?
+
+print(
+'''My name is John
+and I love big pizza pies'''
+)
+
+# Using multiline strings (as denoted by triple single or triple double quotes)
