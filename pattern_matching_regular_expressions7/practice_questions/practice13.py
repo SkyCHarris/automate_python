2abe7a789187e2b0c5b0e832de9338c7ff71d7d2
Sky Harris <skyler.iqeq@gmail.com>
2023-11-30 12:49:37 -0700

    Practice questions 13,14,15 in Regex chapter

---
+++ pattern_matching_regular_expressions7/practice_questions/practice13.py
@@ -0,0 +1,5 @@
+# What do the \D, \W, and \S shorthand character classes signify in regexes?
+
+# \D -> anything that's not a digit
+# \W -> anything that's not a word
+# \S -> anything that's not a space
---
+++ pattern_matching_regular_expressions7/practice_questions/practice15.py
@@ -0,0 +1,3 @@
+# What is the . character class syntax to match all numbers and lowercase letters?
+
+# [a-z0-9]
